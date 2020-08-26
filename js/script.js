e987691089ee893dd3473e65a50d83ae4d21f4be
Eldar Dzhunushaliev <eldar.junushaliev@tucana.aero>
2020-08-26 12:06:51 +0300

    Decoration fixes

--- js/script.js
+++ js/script.js
@@ -1,115 +1,110 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
 'use strict';
 
-//movieDB object
-const movieDB = {
-    movies: [
-        "Joker",
-        "Batman",
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ],
-
-    //Reloading database
-    reloadDB: function() {
-        this.movies.sort();
-        
-        promoList.innerHTML ='';
-        
-        this.movies.forEach((item, index) => { 
-            promoList.innerHTML +=
-                `<li class=promo__interactive-item>${index + 1}. ${item}
-                    <div class="delete"></div>
-                </li>`;
+document.addEventListener('DOMContentLoaded', () => {
+    
+    //sorting without case
+    function sortArr(array) {
+        array.sort(function(a,b) {
+            if (a.toLowerCase() > b.toLowerCase()){return 1;}
+            if (a.toLowerCase() < b.toLowerCase()){return -1;}
+            if (a>b){return 1;}
+            if (a<b){return -1;}
+            return 0;
         });
-        
-        //assigning events to trashcans
-        trashCans = document.querySelectorAll('.delete');
-        trashCans.forEach((item, index) => {
-            item.addEventListener('click', (event) => {
-                delete movieDB.movies[index];
-                event.srcElement.parentElement.remove();
-                movieDB.reloadDB();
+    }
+
+    //movieDB object
+    const movieDB = {
+        movies: [
+            "Joker",
+            "Batman",
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против всех"
+        ],
+    
+        //Reloading database
+        reloadDB: function() {
+            sortArr(movieDB.movies);
+            
+            promoList.innerHTML ='';
+            
+            movieDB.movies.forEach((item, index) => { 
+                promoList.innerHTML +=
+                    `<li class=promo__interactive-item style="text-transform: none;">
+                        ${index + 1}. ${item}
+                        <div class="delete"></div>
+                    </li>`;
             });
-        });        
-    }
-};
+            //assigning events to trashcans
+            trashCans = document.querySelectorAll('.delete');
+            trashCans.forEach((item, index) => {
+                item.addEventListener('click', (event) => {
+                    movieDB.movies.splice(index, 1);
+                    item.parentElement.remove();
 
-//Adding new movie to list function
-function confirmAdding(event) {
-    event.preventDefault();
-
-    newFilm = inputField.value;
-
-    if (newFilm!='') {
-        if (newFilm.length > 21){newFilm = newFilm.slice(0,21) + '...';}
-        
-        if (checkbox.checked){
-            alert('Adding your favourite movie');
-            checkbox.checked = false;
+                    movieDB.reloadDB();
+                });
+            });        
         }
+    };
+    
+    //Adding new movie to list function
+    const confirmAdding = function(event) {
+        event.preventDefault();
+    
+        newFilm = addInput.value;
+    
+        if (newFilm) {
+            //formating data
+            if (newFilm.length > 21){newFilm = `${newFilm.slice(0,21)}...`;}
+            
+            if (addCheck.checked){alert('Adding your favourite movie');}
+    
+            movieDB.movies.push(newFilm);
+            movieDB.reloadDB();
+            
+            event.target.reset();
+            addInput.placeholder = 'Что уже посмотрено...?';
+        } else {
+            addInput.value = '';
+            addInput.placeholder = 'Введите название фильма...';
+            addInput.style.backgroundColor = 'LightCoral';
+            setTimeout(() => {addInput.style.cssText = 'background: rgba(196, 196, 196, 0.29)';}, 500);
+        }
+    };
+    
+    //Variables declaration
+    const advs = document.querySelectorAll('.promo__adv img'),
+          genre = document.querySelector('.promo__genre'),
+          promoBG = document.querySelector('.promo__bg'),
+          promoList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          addCheck = addForm.querySelector('[type="checkbox"]');
+    
+    let newFilm,
+        trashCans = document.querySelectorAll('.delete');
+    
+    //Initialization of input data    
+    movieDB.movies.forEach((item, index) => {
+        if (item.length > 21){item = `${item.slice(0,21)}...`;}
+        movieDB.movies[index] = item;
+    });
+    movieDB.reloadDB();
+    
+    //Removing advertisement
+    advs.forEach((item) => item.remove());
+    
+    //Changing genre to drama
+    genre.textContent = 'драма';
+    
+    //Changing background image
+    promoBG.style.backgroundImage = 'url("img/bg.jpg")';
+    
+    //Adding confirm event to form
+    addForm.addEventListener('submit', confirmAdding);
 
-        movieDB.movies.push(newFilm);
-        movieDB.reloadDB();
-
-        inputField.value = '';
-        inputField.placeholder = 'Что уже посмотрено...?';
-    } else {
-        inputField.value = '';
-        inputField.placeholder = 'Введите название фильма...';
-        inputField.style.backgroundColor = 'LightCoral';
-        setTimeout(() => {inputField.style.cssText = 'background: rgba(196, 196, 196, 0.29)';}, 500);
-    }
-}
-
-//Variables declaration
-const advs = document.querySelectorAll('.promo__adv img'),
-      genre = document.querySelector('.promo__genre'),
-      promoBG = document.querySelector('.promo__bg'),
-      promoListItems = document.querySelectorAll('.promo__interactive-item'),
-      promoList = document.querySelector('.promo__interactive-list'),
-      inputField = document.querySelector('.adding__input'),
-      button = document.getElementsByTagName('button')[0],
-      checkbox = inputField.nextElementSibling.nextElementSibling;
-      
-let newFilm,
-    trashCans = document.querySelectorAll('.delete');
-
-//Initialization of input data
-movieDB.movies.forEach((item, index, array) => {
-    if (item.length > 21){item = item.slice(0,21) + '...';}
 });
-
-//Removing advertisement
-advs.forEach((item) => item.remove());
-
-//Changing genre to drama
-genre.textContent = 'драма';
-
-//Changing background image
-promoBG.style.backgroundImage = 'url("img/bg.jpg")';
-
-//Relodaing database
-movieDB.reloadDB();
-
-//Adding click event to confirm button
-button.addEventListener('click', confirmAdding);
